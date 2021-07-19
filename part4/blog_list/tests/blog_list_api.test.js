60e9b5e780b218eb11145f363ee5e76fa9062369
Daniel Weiler <danielweiler78@gmail.com>
2021-07-19 15:36:03 +0300

    Add tests (and according test files), create scripts for production, development, and test versions, install cross-env for compatability with Windows, install lodash for help with arrays, install supertest, install express-async-errors, and make minor modifications

---
+++ part4/blog_list/tests/blog_list_api.test.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(helper.initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(helper.initialBlogs[1])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+}, 100000)
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'async/await simplifies making async calls',
+    author: 'daniel',
+    url: 'thirdblog',
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  
+  const titles = blogsAtEnd.map(n => n.title)
+  expect(titles).toContain(
+    'async/await simplifies making async calls'
+  )
+})
+
+
+
+afterAll(() => {
+  mongoose.connection.close()
+})
---
+++ part4/blog_list/tests/test_helper.js
@@ -0,0 +1,40 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'test blog post',
+    author: 'daniel',
+    url: 'firstblog',
+    likes: 7
+  },
+  {
+    title: 'another test blog post',
+    author: 'daniel',
+    url: 'secondblog',
+    likes: 8
+  },
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog({
+    title: 'will remove soon',
+    author: 'daniel',
+    url: 'ablog',
+    likes: 1 
+  })
+  await blog.save()
+  await blog.remove()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, 
+  nonExistingId, 
+  blogsInDb
+}
